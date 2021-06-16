f86f61d9f75811684211958f638b7502b51862d2
Long Le Xich <codenamelxl@users.noreply.github.com>
2021-06-16 17:09:03 +0800

    Modify sasl version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     ],
     extras_require={
         'presto': ['requests>=1.0.0'],
-        'hive': ['sasl>=0.2.1', 'thrift>=0.10.0', 'thrift_sasl>=0.1.0'],
+        'hive': ['sasl==0.2.1', 'thrift==0.10.0', 'thrift_sasl==0.3.0'],
         'sqlalchemy': ['sqlalchemy>=1.3.0'],
         'kerberos': ['requests_kerberos>=0.12.0'],
     },
